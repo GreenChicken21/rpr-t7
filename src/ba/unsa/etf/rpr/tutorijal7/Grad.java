ea68063d85c4fe4cde3dd091472b49dc6b547286
GreenChicken21 <dcukurija1@etf.unsa.ba>
2018-12-02 17:51:04 +0100

    nista zivo ne radi

--- src/ba/unsa/etf/rpr/tutorijal7/Grad.java
+++ src/ba/unsa/etf/rpr/tutorijal7/Grad.java
@@ -1,10 +1,10 @@
 package ba.unsa.etf.rpr.tutorijal7;
 
 public class Grad {
-    private String naziv = new String();
+    private String naziv = "";
     private int brojStanovnika = 0;
     private double[] temperatura = new double[1000];
-
+    private int btemp = 0;
 
     public String getNaziv() {
         return naziv;
@@ -26,7 +26,22 @@ public class Grad {
         return temperatura;
     }
 
+    public void setTemperatura(double temperatura)
+    {
+        this.getTemperatura()[getBtemp()] = temperatura;
+        setBtemp(getBtemp() + 1);
+    }
+
+
     public void setTemperatura(double[] temperatura) {
         this.temperatura = temperatura;
+    }
+
+    public int getBtemp() {
+        return btemp;
+    }
+
+    public void setBtemp(int btemp) {
+        this.btemp = btemp;
     }
 }
--- src/ba/unsa/etf/rpr/tutorijal7/Tutorijal.java
+++ src/ba/unsa/etf/rpr/tutorijal7/Tutorijal.java
@@ -1,8 +1,64 @@
 package ba.unsa.etf.rpr.tutorijal7;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Tutorijal {
+    // izdvaja naziv grada
+    public static String izdvojiString(String s){
+        String pov = "";
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i) == ',')return pov;
+            pov += s.charAt(i);
+        }
+        return pov;
+    }
+    // izdvaja temperature u niz
+    private static ArrayList<Integer> izdvojiNiz(String linija) {
+        ArrayList<Integer> pov = new ArrayList<>();
+        Scanner brojevi = new Scanner(linija);
+        while(brojevi.hasNext()){
+            pov.add(brojevi.nextInt());
+        }
+        return pov;
+    }
+
+
+    public static ArrayList<Grad> dodajGradove(){
+
+        Scanner ulaz = null;
+        try{
+            ulaz = new Scanner(new FileReader("mjerenja.txt"));
+        } catch (FileNotFoundException e) {
+            System.out.println("Datoteka ulaz.txt ne postoji ili se ne može otvoriti");
+            return null;
+        }
+
+
+        ArrayList<Grad> gradovi = new ArrayList<>();
+        int brojac = 0;
+        while(ulaz.hasNextLine()){
+
+            gradovi.add(new Grad());
+            String linija = ulaz.nextLine();
+
+            gradovi.get(brojac).setNaziv(izdvojiString(linija));
+
+            ArrayList<Integer> temp = izdvojiNiz(linija);
+            for(int i=0; i<temp.size();i++)
+                gradovi.get(brojac).setTemperatura(temp.get(i));
+
+            brojac++;
+        }
+        return gradovi;
+    }
+
+
 
     public static void main(String[] args) {
-	// write your code here
+        ArrayList<Grad> gradovi = new ArrayList<>(dodajGradove());
+        System.out.println(gradovi.get(0).getNaziv());
     }
 }
