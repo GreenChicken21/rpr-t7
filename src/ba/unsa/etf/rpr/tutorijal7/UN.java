21bce909fc0c6857098c393c58589918b862a4f6
GreenChicken21 <dcukurija1@etf.unsa.ba>
2018-12-02 11:51:10 +0100

    napravljena klasa UN

---
+++ src/ba/unsa/etf/rpr/tutorijal7/UN.java
@@ -0,0 +1,16 @@
+package ba.unsa.etf.rpr.tutorijal7;
+
+import java.util.ArrayList;
+
+public class UN {
+    private ArrayList<Drzava> listaDrzava = new ArrayList<>();
+
+
+    public ArrayList<Drzava> getListaDrzava() {
+        return listaDrzava;
+    }
+
+    public void setListaDrzava(ArrayList<Drzava> listaDrzava) {
+        this.listaDrzava = listaDrzava;
+    }
+}
