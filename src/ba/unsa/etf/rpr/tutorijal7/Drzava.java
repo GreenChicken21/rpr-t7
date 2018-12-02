8313cdc99dd95ea9bd0c948b398f49fdcafdfbea
GreenChicken21 <dcukurija1@etf.unsa.ba>
2018-12-02 11:48:56 +0100

    napravljene klase grad i drzava

--- src/ba/unsa/etf/rpr/tutorijal7/Drzava.java
+++ src/ba/unsa/etf/rpr/tutorijal7/Drzava.java
@@ -1,10 +1,50 @@
 package ba.unsa.etf.rpr.tutorijal7;
 
 public class Drzava {
-    String naziv = new String();
-    int brojStanovnika = 0;
-    double povrsina = 0;
-    String jedinica = new String();
-    Grad glavniGrad = new Grad();
+    private String naziv = new String();
+    private int brojStanovnika = 0;
+    private double povrsina = 0;
+    private String jedinica = new String();
+    private Grad glavniGrad = new Grad();
 
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public int getBrojStanovnika() {
+        return brojStanovnika;
+    }
+
+    public void setBrojStanovnika(int brojStanovnika) {
+        this.brojStanovnika = brojStanovnika;
+    }
+
+    public double getPovrsina() {
+        return povrsina;
+    }
+
+    public void setPovrsina(double povrsina) {
+        this.povrsina = povrsina;
+    }
+
+    public String getJedinica() {
+        return jedinica;
+    }
+
+    public void setJedinica(String jedinica) {
+        this.jedinica = jedinica;
+    }
+
+    public Grad getGlavniGrad() {
+        return glavniGrad;
+    }
+
+    public void setGlavniGrad(Grad glavniGrad) {
+        this.glavniGrad = glavniGrad;
+    }
 }
